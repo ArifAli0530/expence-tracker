dd3450049db09107267ef42d79daf8ad8a505d12
ArifAli0530 <aa9884433@gmail.com>
2025-04-16 08:09:34 +0530

    your descriptive commit message

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const {default : mongoose}= require("mongoose");
 exports.connectDB= async()=>{
     try {
         // const connect = await mongoose.connect(process.env.MONGO_URL);
-        const connect = await mongoose.connect("mongodb+srv://rohit1447481:3TuByNRJGNF0Ox5O@cluster0.ij0ki.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        const connect = await mongoose.connect("mongodb+srv://aa9884433:kaif123@expensetracker.gwujtby.mongodb.net/?retryWrites=true&w=majority&appName=expenseTracker");
 
         if(connect){
             console.log("MongoDb is connected");
